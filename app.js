bebff503f1fb96f05cef904f44e28e14235146c2
thais <thais.de.m.damasio@accenture.com>
2020-05-26 20:51:47 -0300

    added routes

--- app.js
+++ app.js
@@ -3,21 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-//mais específico primeiro, porque senão a url vai dar match com somente o / (que é o que começa), e vai chamar a função que lida com /
-
-app.use("/add-product",(req, res) =>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-});
-
-app.post('/product', (req,res) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use("/",(req,res,next) =>{
-    res.send("<h1>Bem vindo</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
