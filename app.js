38a34417a6f90bc18b50452edc2822d137734617
thais <thais.de.m.damasio@accenture.com>
2020-05-26 20:40:36 -0300

    method post

--- app.js
+++ app.js
@@ -3,37 +3,21 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
-
-// app.use((req,res,next) =>{
-//     console.log("Ola")
-//     next();
-// })
-
-// app.use((req,res,next) =>{
-//     console.log("Tudo bem?")
-//     res.send("<h1>Oiiii</h1>")
-// })
+app.use(bodyParser.urlencoded({extended: false}));
 
 //mais específico primeiro, porque senão a url vai dar match com somente o / (que é o que começa), e vai chamar a função que lida com /
-app.use("/users",(req,res,next) =>{
-    res.send("<h1>Sem usuários no momento</h1>");
-    next();
+
+app.use("/add-product",(req, res) =>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
-app.use("/add-product",(req,res,next) =>{
-    console.log("in another middleware");
-    res.send('<form action="/product method="POST"><input type="text" name="title"<button type="submit">Add Product</button></form>')
-});
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body)
+app.post('/product', (req,res) => {
+    console.log(req.body);
     res.redirect('/');
 })
 
 app.use("/",(req,res,next) =>{
-    console.log("Ola")
-    res.send("<h1>Bem vindo</h1>")
+    res.send("<h1>Bem vindo</h1>");
 });
 
 app.listen(3000);
