b6a4b76ce30c751f1ecdce4a34c08cbe419939cf
thais <thais.de.m.damasio@accenture.com>
2020-05-26 20:57:21 -0300

    added page not found

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+// ordem nao importa por causa do método que esta sendo usado
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req,res,next) =>{
+    res.status(404).send('<h1>Page not found!</h1>')
+});
 
 app.listen(3000);
