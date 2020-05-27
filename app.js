b75d75d6aff561e7037554a9e54045ef6766c8cd
thais <thais.de.m.damasio@accenture.com>
2020-05-26 22:28:18 -0300

    added public folder

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ordem nao importa por causa do método que esta sendo usado
 app.use('/admin',adminRoutes);
