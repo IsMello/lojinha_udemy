3a4750393f2cea2efc59c5e634ea126f28d031b2
thais <thais.de.m.damasio@accenture.com>
2020-05-26 21:34:48 -0300

    added function for the root file of the app

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path')
+
 const router = express.Router();
 
 router.get("/",(req,res,next) =>{
     //use path.join to create path that work all OS
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, '..', 'views', 'shop.html'));
 });
 
 module.exports = router;
