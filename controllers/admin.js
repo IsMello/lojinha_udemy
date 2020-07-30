9d0e91ab2450e5b1e139f3a5c8b47506219dc054
thais <thais.de.m.damasio@accenture.com>
2020-07-30 17:40:00 -0300

    added module 12

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,13 +14,17 @@ exports.pushProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl
   const price = req.body.price
   const description = req.body.description
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description
-    })
+  const userId = req.user._id
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    userId
+  )
+  product
+    .save()
     .then(result => {
       // console.log(result)
       console.log('Created product!')
@@ -37,12 +41,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/')
   }
   const prodId = req.params.productId
-  req.user
-    .getProducts({ where: { id: prodId } })
-    // Product.findByPk(prodId)
-    .then(products => {
-      const product = products[0]
-      if (!products) {
+  Product.findById(prodId)
+    .then(product => {
+      if (!product) {
         return res.redirect('/')
       }
       res.render('admin/edit-product', {
@@ -63,16 +64,17 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl
   const updatedPrice = req.body.price
   const updatedDescription = req.body.description
-  Product.findByPk(prodId)
-    .then(product => {
-      product.title = updatedTile
-      product.price = updatedPrice
-      product.description = updatedDescription
-      product.imageUrl = updatedImageUrl
-      return product.save()
-    })
+
+  const product = new Product(
+    updatedTile,
+    updatedPrice,
+    updatedDescription,
+    updatedImageUrl,
+    prodId
+  )
+  product
+    .save()
     .then(result => {
-      // porque dois .then?
       console.log('UPDATED PRODUCT')
       res.redirect('/admin/products')
     })
@@ -82,8 +84,7 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
+  Product.fetchAll()
     .then(rows => {
       res.render('admin/products', {
         products: rows,
@@ -98,11 +99,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const productId = req.body.productId
-  Product.findByPk(productId)
-    .then(product => {
-      return product.destroy()
-    })
-    .then(result => {
+  Product.deleteById(productId)
+    .then(() => {
       console.log('DESTROY')
       res.redirect('/admin/products')
     })
--- models/user.js
+++ models/user.js
@@ -1,22 +1,138 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const getDb = require('../util/database').getDb
 
-const sequelize = require('../util/database')
+const ObjectId = mongodb.ObjectId
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
+class User {
+  constructor (username, email, cart, id) {
+    this.name = username
+    this.email = email
+    this.cart = cart
+    this._id = id
   }
-})
+
+  save () {
+    const db = getDb()
+    return db
+      .collection('users')
+      .insertOne(this)
+      .then(result => {
+        console.log('User created')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  addToCart (product) {
+    const cartProductIndex = this.cart.items.findIndex(cartprod => {
+      return cartprod.productId.toString() === product._id.toString()
+    })
+    let newQuantity = 1
+    const updatedCartItems = [...this.cart.items]
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1
+      updatedCartItems[cartProductIndex].quantity = newQuantity
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity
+      })
+    }
+    const updatedCart = {
+      items: updatedCartItems
+    }
+    const db = getDb()
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      )
+  }
+  // importante! //
+
+  getCart () {
+    const db = getDb()
+    const productIds = this.cart.items.map(item => {
+      return item.productId
+    })
+    return db
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then(products => {
+        return products.map(product => {
+          return {
+            ...product,
+            quantity: this.cart.items.find(item => {
+              return item.productId.toString() === product._id.toString()
+            }).quantity
+          }
+        })
+      })
+  }
+
+  deleteItemFromCart (productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString() // false = cart sem o item que deve ser deletado ?
+    })
+    const db = getDb()
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      )
+  }
+
+  addOrder () {
+    const db = getDb()
+    return this.getCart()
+      .then(products => {
+        const order = {
+          items: products,
+          user: {
+            _id: new mongodb.ObjectId(this._id),
+            name: this.name,
+            email: this.email
+          }
+        }
+        return db.collection('orders').insertOne(order)
+      })
+      .then(result => {
+        this.cart = { items: [] }
+        return db
+          .collection('users')
+          .updateOne(
+            { _id: new mongodb.ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          )
+      })
+  }
+
+  getOrders () {
+    const db = getDb()
+    return db
+      .collection('orders')
+      .find({ 'user._id': new mongodb.ObjectId(this._id) })
+      .toArray()
+  }
+
+  static findById (userId) {
+    const db = getDb()
+    return db
+      .collection('users')
+      .findOne({ _id: new ObjectId(userId) })
+      .then(user => {
+        console.log(user)
+        return user
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
 
 module.exports = User
